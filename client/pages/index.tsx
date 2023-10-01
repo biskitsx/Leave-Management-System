8df3975f7923bb7585e5036ac483c4373deafbf3
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-10-01 17:13:29 +0700

    add useEffect and write css

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,20 +1,29 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Nav from '../components/Nav'
-import { useEffect } from 'react'
+import { use, useEffect } from 'react'
 import LeaveCount from '../components/LeaveCount'
 import LeaveTable from '../components/LeaveTable'
 import AddLeaveBtn from '../components/AddLeaveBtn'
+import { useSelector } from 'react-redux'
 
 
 export default function Home() {
+    const { user } = useSelector((state) => state.user)
     useEffect(() => {
+        const { user } = JSON.parse(localStorage.getItem('user') || '{}')
+        if (!user) {
+            window.location.href = '/login'
+        }
     }, [])
     return (
         <div className='p-2 flex flex-col gap-10 items-center'>
             <Nav />
             <div className='flex flex-col gap-10 w-8/12'>
-                <h1 className='text-4xl font-bold text-center'>ยินดีต้อนรับ 👋🏻</h1>
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-4xl font-bold text-center'>ยินดีต้อนรับ <span className='text-blue-700'>{user?.first_name}</span> 👋🏻</h1>
+                    <h1 className='text-xl font-normal text-center'>สรุปยอดการลาทั้งหมด</h1>
+                </div>
                 <LeaveCount />
                 <AddLeaveBtn />
                 <LeaveTable />
