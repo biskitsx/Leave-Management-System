0cd16ea019a877d36adedb5a35565eb757241a52
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-10-01 17:13:00 +0700

    add useEffect and logout button

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { login } from '../store/user'
+import { cookies } from 'next/headers'
+import axios from 'axios'
 function Nav() {
     const { user } = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        const { user } = JSON.parse(localStorage.getItem('user') || '{}')
+        if (user) {
+            dispatch(login(user))
+        }
+    }, [])
+
+    const logout = async () => {
+        const res = await axios.post("http://localhost:8000/auth/logout", {}, { withCredentials: true })
+        localStorage.removeItem('user')
+        dispatch(login(null))
+        window.location.href = '/login'
+    }
     return (
         <div className="navbar bg-base-100 shadow-md rounded-xl">
             <div className="flex-1">
@@ -9,16 +27,16 @@ function Nav() {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
                     <li>
                         <details>
-                            <summary>
-                                {user && user.first_name}
+                            <summary className='font-bold text-lg text-blue-700'>
+                                {user ? user.first_name : "Login Now"}
                             </summary>
-                            <ul className="p-2 bg-base-100">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
+                            {user &&
+                                <ul className="p-2 bg-base-100">
+                                    <li><button onClick={logout}>Logout</button></li>
+                                </ul>
+                            }
                         </details>
                     </li>
                 </ul>
