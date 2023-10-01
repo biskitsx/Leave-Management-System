97575e96ee96d39a613f79a3e6e60526890a453d
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-10-01 17:12:35 +0700

    add fetch Leaves

--- client/components/LeaveTable.tsx
+++ client/components/LeaveTable.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { LeaveDTO } from '../store/leave'
+import React, { use, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { LeaveDTO, setLeave } from '../store/leave'
+import axios from 'axios'
 function LeaveTable() {
     const { leave } = useSelector((state) => state.leave)
+    console.log(leave)
+    console.log(leave.leave_responses)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        const fetchLeave = async () => {
+            const res = await axios.get('http://localhost:8000/leaves/me', { withCredentials: true })
+            console.log(res.data)
+            const data = res.data
+            dispatch(setLeave(data))
+        }
+        fetchLeave()
+    }, [])
+
     return (
-        <table className="table bg-base-100 overflow-hidden shadow-md text-zinc-700">
+        <table className="table bg-base-100 overflow-hidden shadow-md text-zinc-700 text-center">
             {/* head */}
             <thead>
                 <tr className='text-lg text-zinc-700 bg-base-300'>
@@ -17,17 +31,35 @@ function LeaveTable() {
                     <th>หมายเหตุ</th>
                 </tr>
             </thead>
-            <tbody className='text-lg font-normal'>
+            <tbody className='text-md font-normal'>
                 {/* row 1 */}
-                {leave && leave.map((item: LeaveDTO, index: number) => {
+                {leave && leave?.leave_responses?.map((item: LeaveDTO, index: number) => {
+                    let color = ''
+                    if (item.type == 'ลาป่วย') {
+                        color = "rounded-md text-white bg-red-500"
+                    } else if (item.type == 'ลากิจ') {
+                        color = "rounded-md text-white bg-yellow-500"
+                    } else if (item.type == 'ลาพักร้อน') {
+                        color = "rounded-md text-white bg-blue-500"
+                    }
+
+                    let date = new Date(item.created_at)
+                    let newDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
+
+                    let startDate = new Date(item.time_start)
+                    let newStartDate = startDate.getDate() + '/' + (startDate.getMonth() + 1) + '/' + startDate.getFullYear()
+
+                    let endDate = new Date(item.time_end)
+                    let newEndDate = endDate.getDate() + '/' + (endDate.getMonth() + 1) + '/' + endDate.getFullYear()
+
                     return (
                         <tr key={index} className=''>
                             <td>{index + 1}</td>
-                            <td>{item.type}</td>
-                            <td>3</td>
-                            <td>{item.time_start}</td>
-                            <td>{item.time_end}</td>
-                            <td>{item.created_at}</td>
+                            <td ><p className={color}>{item.type}</p></td>
+                            <td>{item.leave_day}</td>
+                            <td>{newStartDate}</td>
+                            <td>{newEndDate}</td>
+                            <td>{newDate}</td>
                             <td>{item.detail}</td>
                         </tr>
                     )
